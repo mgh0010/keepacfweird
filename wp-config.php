75c8e1086c1842d2458df9209c0a9b66ea15ca3a
Michaelguyson <mghharrison@gmail.com>
2017-04-18 23:47:47 +0000

    making deployable

--- wp-config.php
+++ wp-config.php
@@ -22,17 +22,38 @@ if (isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) && $_SERVER["HTTP_X_FORWARDED_PROT
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
+/** The name of the database for WordPress *
 define('DB_NAME', 'c9');
 
-/** MySQL database username */
+/** MySQL database username *
 define('DB_USER', substr(getenv('C9_USER'), 0, 16));
 
-/** MySQL database password */
+/** MySQL database password *
 define('DB_PASSWORD', '');
 
-/** MySQL hostname */
+/** MySQL hostname *
 define('DB_HOST', getenv('IP'));
+
+/** Database Charset to use in creating database tables. *
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. *
+define('DB_COLLATE', '');
+
+// ** MySQL settings - You can get this info from your web host */
+$url = parse_url(getenv('DATABASE_URL') ? getenv('DATABASE_URL') : getenv('CLEARDB_DATABASE_URL'));
+
+/** The name of the database for WordPress */
+define('DB_NAME', trim($url['path'], '/'));
+
+/** MySQL database username */
+define('DB_USER', $url['user']);
+
+/** MySQL database password */
+define('DB_PASSWORD', $url['pass']);
+
+/** MySQL hostname */
+define('DB_HOST', $url['host']);
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -49,15 +70,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
-
+define('AUTH_KEY',         getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY',        getenv('NONCE_KEY'));
+define('AUTH_SALT',        getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT',       getenv('NONCE_SALT'));
 /**#@-*/
 
 /**
